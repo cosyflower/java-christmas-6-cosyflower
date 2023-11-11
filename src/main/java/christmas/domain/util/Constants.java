4037947aa35a8de15118202ac32123d914357654
sunghun <nokdonwithkj211@gmail.com>
2023-11-11 11:03:18 +0900

    fix(Constants): ORDER_PATTERN 수정

--- src/main/java/christmas/domain/util/Constants.java
+++ src/main/java/christmas/domain/util/Constants.java
@@ -10,5 +10,5 @@ public class Constants {
     public static final int INIT_VALUE = 0;
 
     public static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]+$");
-    public static final Pattern ORDER_PATTERN = Pattern.compile("^([ㄱ-ㅎ가-힣]+-[0-9]+,?)+$");
+    public static final Pattern ORDER_PATTERN = Pattern.compile("^([ㄱ-ㅎ가-힣]+-[0-9]+)(,[ㄱ-ㅎ가-힣]+-[0-9]+)*$");
 }
--- src/test/java/christmas/domain/OrderDTOTest.java
+++ src/test/java/christmas/domain/OrderDTOTest.java
@@ -11,11 +11,9 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class OrderDTOTest {
-
     @Nested
     @DisplayName("예외가 발생하는 경우")
     class unvalidOrder {
-
         @ParameterizedTest
         @NullAndEmptySource
         void 아무것도_입력하지_않은_경우_예외(String unvalidOrder) {
@@ -23,14 +21,38 @@ public class OrderDTOTest {
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("아무것도 입력하지");
         }
+
+        @ParameterizedTest(name = "$메뉴명-$메뉴 개수의 형식을 지키지 않은 경우")
+        @ValueSource(strings = {"양송이스프2개", "티본스테이크 - 2개", "티본스테이크-2개"})
+        void 형식에_맞지_않는_경우_예외(String nonPatternOrder) {
+            Assertions.assertThatThrownBy(() -> new OrderDTO(nonPatternOrder))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("유효하지 않은 주문입니다");
+        }
+
+        @ParameterizedTest(name = "형식에 맞게 입력했으나 복수의 메뉴를 주문시 구분자를 활용하지 않은 경우")
+        @ValueSource(strings = {"양송이스프-2티본스테이크-3"})
+        void 구분자가_없는_경우_예외(String nonSeparator) {
+            Assertions.assertThatThrownBy(() -> new OrderDTO(nonSeparator))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("유효하지 않은 주문입니다");
+        }
+
+        @ParameterizedTest(name = "형식에 맞게 입력했으나 복수의 메뉴를 주문시 정해진 구분자 콤마(',')가 아닌 다른 문자를 명시한 경우")
+        @ValueSource(strings = {"양송이스프-2|스테이크-3", "스테이크-3:양송이스프-2", "양송이스프-2 스테이크-3"})
+        void 주문_형식에_맞게_입력하면_성공(String wrongSeparator) {
+            Assertions.assertThatThrownBy(() -> new OrderDTO(wrongSeparator))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("유효하지 않은 주문입니다");
+        }
     }
 
     @Nested
     @DisplayName("예외가 발생하지 않는 경우")
     class validOrder {
         @ParameterizedTest
-        @ValueSource(strings = {"양송이스프-2", "스테이크-3", "양송이스프-2,스테이크-3"})
-        void 주문_형식에_맞게_입력하면_성공(String validOrder) {
+        @ValueSource(strings = "양송이스프-2,티본스테이크-3")
+        void 형식에_맞게_입력하면_성공(String validOrder) {
             assertThatCode(() -> new OrderDTO(validOrder))
                     .doesNotThrowAnyException();
         }

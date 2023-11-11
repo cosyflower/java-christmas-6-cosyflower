9e9ba2baecbf7b7335ddaad000af041fa396be9c
sunghun <nokdonwithkj211@gmail.com>
2023-11-11 10:23:28 +0900

    feat(OrderDTO, OrderDTOTest): 1차 검증을 담당하는 OrderDTO 생성

---
+++ src/test/java/christmas/domain/OrderDTO.java
@@ -0,0 +1,33 @@
+package christmas.domain;
+
+import christmas.Constants;
+
+public class OrderDTO {
+    private final String menuAndNumber;
+
+    public OrderDTO(String menuAndNumber) {
+        validate(menuAndNumber);
+        this.menuAndNumber = menuAndNumber;
+    }
+
+    private void validate(String menuAndNumber) {
+        isNullOrEmpty(menuAndNumber);
+        isOrderPattern(menuAndNumber);
+    }
+
+    private void isNullOrEmpty(String order) {
+        if (order == null || order.isEmpty()) {
+            throw new IllegalArgumentException("아무것도 입력하지 않았습니다");
+        }
+    }
+
+    private void isOrderPattern(String order) {
+        if (Constants.ORDER_PATTERN.matcher(order).matches() == false) {
+            throw new IllegalArgumentException("유효하지 않은 주문입니다.");
+        }
+    }
+
+    public String getMenuAndNumber() {
+        return menuAndNumber;
+    }
+}
---
+++ src/test/java/christmas/domain/OrderDTOTest.java
@@ -0,0 +1,38 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class OrderDTOTest {
+
+    @Nested
+    @DisplayName("예외가 발생하는 경우")
+    class unvalidOrder {
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void 아무것도_입력하지_않은_경우_예외(String unvalidOrder) {
+            Assertions.assertThatThrownBy(() -> new OrderDTO(unvalidOrder))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("아무것도 입력하지");
+        }
+    }
+
+    @Nested
+    @DisplayName("예외가 발생하지 않는 경우")
+    class validOrder {
+        @ParameterizedTest
+        @ValueSource(strings = {"양송이스프-2", "스테이크-3", "양송이스프-2,스테이크-3"})
+        void 주문_형식에_맞게_입력하면_성공(String validOrder) {
+            assertThatCode(() -> new OrderDTO(validOrder))
+                    .doesNotThrowAnyException();
+        }
+    }
+
+}

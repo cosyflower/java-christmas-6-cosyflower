e7ef0b9382b3a3866aabd06d15829e554d763060
sunghun <nokdonwithkj211@gmail.com>
2023-11-13 09:04:48 +0900

    test(DiscountEventTest): 날짜 정보와 해당 날짜에 유효한 이벤트인지 확인하는 테스트로 수정

--- src/test/java/christmas/domain/DiscountEventTest.java
+++ src/test/java/christmas/domain/DiscountEventTest.java
@@ -1,10 +1,11 @@
 package christmas.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import christmas.domain.discount.DiscountEvent;
-import christmas.domain.discount.DiscountPrice;
+import christmas.domain.order.AcceptedOrders;
+import christmas.domain.order.Order;
+import java.util.List;
 import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -12,30 +13,28 @@ import org.junit.jupiter.params.provider.MethodSource;
 public class DiscountEventTest {
     private static Stream<Arguments> dayAndEvent() {
         return Stream.of(
-                Arguments.of(25, DiscountEvent.WEEKDAY_EVENT, 2023),
-                Arguments.of(25, DiscountEvent.WEEKEND_EVENT, 0),
-                Arguments.of(25, DiscountEvent.SPECIAL_EVENT, 100),
-                Arguments.of(25, DiscountEvent.CHRISTMAS_EVENT, 3400),
-                Arguments.of(25, DiscountEvent.PROMOTION_EVENT, 25000),
+                Arguments.of(25, DiscountEvent.WEEKDAY_EVENT, true),
+                Arguments.of(25, DiscountEvent.WEEKEND_EVENT, false),
+                Arguments.of(25, DiscountEvent.SPECIAL_EVENT, true),
+                Arguments.of(25, DiscountEvent.CHRISTMAS_EVENT, true),
+                Arguments.of(25, DiscountEvent.PROMOTION_EVENT, true),
 
-                Arguments.of(22, DiscountEvent.WEEKDAY_EVENT, 0),
-                Arguments.of(22, DiscountEvent.WEEKEND_EVENT, 2023),
-                Arguments.of(22, DiscountEvent.SPECIAL_EVENT, 0),
-                Arguments.of(22, DiscountEvent.CHRISTMAS_EVENT, 3100),
-                Arguments.of(22, DiscountEvent.PROMOTION_EVENT, 25000)
+                Arguments.of(22, DiscountEvent.WEEKDAY_EVENT, false),
+                Arguments.of(22, DiscountEvent.WEEKEND_EVENT, true),
+                Arguments.of(22, DiscountEvent.SPECIAL_EVENT, false),
+                Arguments.of(22, DiscountEvent.CHRISTMAS_EVENT, true),
+                Arguments.of(22, DiscountEvent.PROMOTION_EVENT, true)
         );
 
     }
 
     @ParameterizedTest(name = " 날짜 : {0} 이벤트 : {1} 결과 : {2} ")
     @MethodSource("dayAndEvent")
-    void WeekDay(int dayValue, DiscountEvent discountEvent, int discountPrice) {
+    void WeekDay(int dayValue, DiscountEvent discountEvent, Boolean validOrUnvalid) {
         Day day = Day.from(dayValue);
+        boolean isValid = discountEvent.isValidEvent(day);
 
-        DiscountPrice weekDayDiscount = discountEvent.getDiscountPrice(day);
-
-        assertThat(weekDayDiscount.getDiscountPriceValue()).isEqualTo(discountPrice);
+        Assertions.assertThat(isValid).isEqualTo(validOrUnvalid);
     }
 
-    // weekday, weekend 구분해서 데이터 전달하는 방향으로 작성하기
 }
